f0d969eba1840362daad9debc93907c270b22ea5
Bjørn Lindeijer <bjorn@lindeijer.nl>
2007-03-03 12:02:41 +0000

    Enable the Id keyword.

--- src/utils/mathutils.cpp
+++ src/utils/mathutils.cpp
@@ -17,7 +17,7 @@
  *  with The Mana  World; if not, write to the  Free Software Foundation, Inc.,
  *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- *  $Id: $
+ *  $Id$
  */
 
 #include "mathutils.h"
