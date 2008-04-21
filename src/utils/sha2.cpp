f7fcde7653b352cd4c8c921f01e5f3c99ee80dbb
Bjørn Lindeijer <bjorn@lindeijer.nl>
2008-04-21 17:16:29 +0000

    Removed our copyright header since these files are not copyright by us.

--- src/utils/sha2.cpp
+++ src/utils/sha2.cpp
@@ -1,27 +1,3 @@
-/*
- *  The Mana World
- *  Copyright 2008 The Mana World Development Team
- *
- *  This file is part of The Mana World.
- *
- *  The Mana World is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  any later version.
- *
- *  The Mana World is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with The Mana World; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *  Additional information about this file can be read below:
- *  $Id:$
- */
-
 /*************************************************************
 
     This program is a C++ implementation of the Secure Hash Algorithm (SHA)
