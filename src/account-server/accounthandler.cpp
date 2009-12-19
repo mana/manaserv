71112c8d1bc6d7f0e46868683110088e914c2fe9
Chuck Miller <shadowmil@gmail.com>
2009-12-18 22:47:05 -0500

    Instead of sending http://update.manasource.org/ don't send any update host if defaultUpdateHost is not set

--- src/account-server/accounthandler.cpp
+++ src/account-server/accounthandler.cpp
@@ -255,7 +255,9 @@ void AccountHandler::handleLoginMessage(AccountClient &client, MessageIn &msg)
     client.status = CLIENT_CONNECTED;
 
     reply.writeByte(ERRMSG_OK);
-    reply.writeString(Configuration::getValue("defaultUpdateHost","http://update.manasource.org"));
+    std::string updateHost = Configuration::getValue("defaultUpdateHost","");
+    if (updateHost != "")
+        reply.writeString(updateHost);
     client.send(reply); // Acknowledge login
 
     // Return information about available characters
