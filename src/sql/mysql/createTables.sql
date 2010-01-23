mana/manaserv

commit bb7866f0f5046c82f73cf625adbdeaf101920cbb

Updates mysql to database version 8

src/sql/mysql/createTables.sql
@@ -89,7 +89,19 @@ CREATE TABLE IF NOT EXISTS `mana_char_status_effects` (
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8;
 
+-- Create table 'mana_char_kill_stats'
 
+CREATE TABLE IF NOT EXISTS `mana_char_kill_stats`
+(
+    `char_id` int(11) NOT NULL,
+    `monster_id` int(11) NOT NULL,
+    `kills` int(11),
+    PRIMANY KEY (`char_id`, `monster_id`),
+    FOREIGN KEY (`char_id`)
+        REFERENCES `mana_characters` (`id`)
+        ON DELETE CASCADE
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8;
 
 
 --
@@ -371,7 +383,7 @@ AUTO_INCREMENT=0 ;
 
 INSERT INTO mana_world_states VALUES('accountserver_startup',NULL,NULL, NOW());
 INSERT INTO mana_world_states VALUES('accountserver_version',NULL,NULL, NOW());
-INSERT INTO mana_world_states VALUES('database_version',     NULL,'7',  NOW());
+INSERT INTO mana_world_states VALUES('database_version',     NULL,'8',  NOW());
 
 -- all known transaction codes
 

src/sql/mysql/updates/update_7_to_8.sql
@@ -1,0 +1,17 @@
+
+-- Create table 'mana_char_kill_stats'
+
+CREATE TABLE IF NOT EXISTS `mana_char_kill_stats`
+(
+    `char_id` int(11) NOT NULL,
+    `monster_id` int(11) NOT NULL,
+    `kills` int(11),
+    PRIMANY KEY (`char_id`, `monster_id`),
+    FOREIGN KEY (`char_id`)
+        REFERENCES `mana_characters` (`id`)
+        ON DELETE CASCADE
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8;
+
+UPDATE mana_world_states SET value = '8' WHERE state_name = 'database_version';
+

commit d4097807182cc40f53365b13ef61f4bbd51f473e

Fix a syntax error in MYSQL createTable script
Introduced in commit bb7866f0

src/sql/mysql/createTables.sql
@@ -93,10 +93,10 @@ DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `mana_char_kill_stats`
 (
-    `char_id` int(11) NOT NULL,
-    `monster_id` int(11) NOT NULL,
-    `kills` int(11),
-    PRIMANY KEY (`char_id`, `monster_id`),
+    `char_id` int(10) unsigned NOT NULL,
+    `monster_id` int(10) unsigned NOT NULL,
+    `kills` int(10) NULL,
+    PRIMARY KEY (`char_id`, `monster_id`),
     FOREIGN KEY (`char_id`)
         REFERENCES `mana_characters` (`id`)
         ON DELETE CASCADE