074351e17125998d9de19d9528b08ab0b2029870
Yohann Ferreira <yohann_dot_ferreira_at_orange_dot_efer>
2011-01-27 01:38:09 +0100

    Simply fixing the map script header while i see it.

--- example/serverdata/scripts/maps/desert.lua
+++ example/serverdata/scripts/maps/desert.lua
@@ -1,11 +1,11 @@
 ----------------------------------------------------------
--- Tulimshar Bazaar                                     --
+-- Template script for the Desert map                   --
 ----------------------------------------------------------------------------------
---  Copyright 2008 The Mana World Development Team                              --
+--  Copyright 2011 The Mana Development Team                                    --
 --                                                                              --
---  This file is part of The Mana World.                                        --
+--  This file is part of Manasource Project.                                    --
 --                                                                              --
---  The Mana World  is free software; you can redistribute  it and/or modify it --
+--  Manasource is free software; you can redistribute  it and/or modify it      --
 --  under the terms of the GNU General  Public License as published by the Free --
 --  Software Foundation; either version 2 of the License, or any later version. --
 ----------------------------------------------------------------------------------
