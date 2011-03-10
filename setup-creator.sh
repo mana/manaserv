f4c1ea6880ea4897a6973fe08b1ef86644b9e447
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2011-03-10 22:12:04 +0100

    Added a script to create and update a Qt Creator project

---
+++ setup-creator.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# This script creates (and updates) a Qt Creator project file, for developing
+# manaserv using the Qt Creator IDE.
+
+echo "[General]" > manaserv.creator
+echo "#define SQLITE_SUPPORT 1" > manaserv.config
+echo "src
+/usr/include/libxml2" > manaserv.includes
+
+git ls-files \*.cpp \
+             \*.h \
+             \*.txt \
+             \*.xml \
+             \*.pro \
+    > manaserv.files
