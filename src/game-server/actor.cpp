mana/manaserv

commit 97e0a9eb170499e045c9cec23fcb2d474079c7dd

Fixed Actor::setPosition to update the blockmask correctly
It was freeing the new tile rather than the previous tile.

Reviewed-by: Stefan Dombrowski

src/game-server/actor.cpp
@@ -27,23 +27,21 @@
 
 void Actor::setPosition(const Point &p)
 {
-    mPos = p;
-
     // Update blockmap
-    if (getMap())
+    if (Map *map = getMap()->getMap())
     {
-        Map *map = getMap()->getMap();
         int tileWidth = map->getTileWidth();
         int tileHeight = map->getTileHeight();
-        const Point &oldP = getPosition();
-        if ((oldP.x / tileWidth != p.x / tileWidth
-            || oldP.y / tileHeight != p.y / tileHeight))
+        if ((mPos.x / tileWidth != p.x / tileWidth
+            || mPos.y / tileHeight != p.y / tileHeight))
         {
-            map->freeTile(oldP.x / tileWidth, oldP.y / tileHeight,
+            map->freeTile(mPos.x / tileWidth, mPos.y / tileHeight,
                           getBlockType());
             map->blockTile(p.x / tileWidth, p.y / tileHeight, getBlockType());
         }
     }
+
+    mPos = p;
 }
 
 void Actor::setMap(MapComposite *mapComposite)

commit ba57aa3eedf09f32a1ed003ce26ca54666796aef

Moved freeing of map position to Actor destructor
It was done in both the Character and the Monster destructors, but I
don't see how any Actor should be excluded from this. Now it also
happens for NPC, Effect and Item, though only NPC has a relevant
walkmask.

Also fixed a small issue introduced in 97e0a9eb170499 and added an
assert to freeTile. We should be able to assert that a tile can only be
freed if it was blocked.

Reviewed-by: Stefan Dombrowski

src/game-server/actor.cpp
@@ -25,11 +25,25 @@
 
 #include <cassert>
 
+Actor::~Actor()
+{
+    // Free the map position
+    if (MapComposite *mapComposite = getMap())
+    {
+        Map *map = mapComposite->getMap();
+        int tileWidth = map->getTileWidth();
+        int tileHeight = map->getTileHeight();
+        Point oldP = getPosition();
+        map->freeTile(oldP.x / tileWidth, oldP.y / tileHeight, getBlockType());
+    }
+}
+
 void Actor::setPosition(const Point &p)
 {
     // Update blockmap
-    if (Map *map = getMap()->getMap())
+    if (MapComposite *mapComposite = getMap())
     {
+        Map *map = mapComposite->getMap();
         int tileWidth = map->getTileWidth();
         int tileHeight = map->getTileHeight();
         if ((mPos.x / tileWidth != p.x / tileWidth
@@ -46,11 +60,10 @@ void Actor::setPosition(const Point &p)
 
 void Actor::setMap(MapComposite *mapComposite)
 {
-    assert (mapComposite);
-    MapComposite *oldMapComposite = getMap();
-    Point p = getPosition();
+    assert(mapComposite);
+    const Point p = getPosition();
 
-    if (oldMapComposite)
+    if (MapComposite *oldMapComposite = getMap())
     {
         Map *oldMap = oldMapComposite->getMap();
         int oldTileWidth = oldMap->getTileWidth();

src/game-server/actor.h
@@ -58,6 +58,8 @@ class Actor : public Thing
             mSize(0)
         {}
 
+        ~Actor();
+
         /**
          * Sets the coordinates. Also updates the walkmap of the map the actor
          * is on.

src/game-server/character.cpp
@@ -651,18 +651,6 @@ void Character::disconnected()
     }
 }
 
-Character::~Character()
-{
-    if (getMap())
-    {
-        Map *map = getMap()->getMap();
-        int tileWidth = map->getTileWidth();
-        int tileHeight = map->getTileHeight();
-        Point oldP = getPosition();
-        map->freeTile(oldP.x / tileWidth, oldP.y / tileHeight, getBlockType());
-    }
-}
-
 void Character::giveSpecial(int id)
 {
     if (mSpecials.find(id) == mSpecials.end())

src/game-server/character.h
@@ -61,8 +61,6 @@ class Character : public Being
          */
         Character(MessageIn &msg);
 
-        ~Character();
-
         /**
          * recalculates the level when necessary and calls Being::update
          */

src/game-server/map.cpp
@@ -157,8 +157,9 @@ void Map::freeTile(int x, int y, BlockType type)
         return;
 
     MetaTile &metaTile = mMetaTiles[x + y * mWidth];
+    assert(metaTile.occupation[type] > 0);
 
-    if (metaTile.occupation[type] > 0 && !(--metaTile.occupation[type]))
+    if (!(--metaTile.occupation[type]))
     {
         switch (type)
         {

src/game-server/monster.cpp
@@ -133,16 +133,6 @@ Monster::~Monster()
     {
         i->first->removeListener(&mTargetListener);
     }
-
-    // Free the map position
-    if (getMap())
-    {
-        Point oldP = getPosition();
-        Map *map = getMap()->getMap();
-        int tileWidth = map->getTileWidth();
-        int tileHeight = map->getTileHeight();
-        map->freeTile(oldP.x / tileWidth, oldP.y / tileHeight, getBlockType());
-    }
 }
 
 void Monster::perform()