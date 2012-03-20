mana/manaserv

commit ba66fbeda8ef9afb6c33eba66d109bac85ebf628

Added get_character_by_name lua bind
Step to be able to handle chatcommands by scripts.

Reviewed-by: bjorn.

src/game-server/gamehandler.cpp
@@ -383,16 +383,17 @@ void GameHandler::deletePendingConnect(Character *character)
     delete character;
 }
 
-GameClient *GameHandler::getClientByNameSlow(const std::string &name) const
+Character *GameHandler::getCharacterByNameSlow(const std::string &name) const
 {
     for (NetComputers::const_iterator i = clients.begin(),
          i_end = clients.end(); i != i_end; ++i)
     {
         GameClient *c = static_cast< GameClient * >(*i);
         Character *ch = c->character;
-        if (ch && ch->getName() == name)
+        if (ch && ch->getName() == name &&
+                c->status != CLIENT_CONNECTED)
         {
-            return c;
+            return ch;
         }
     }
     return 0;

src/game-server/command.cpp
@@ -356,7 +356,7 @@ void runCommand(Character *ch, const std::string &text)
                 }
                 else
                 {
-                    GameClient *c = gameHandler->getClientByNameSlow(arg);
+                    Character *c = gameHandler->getCharacterByNameSlow(arg);
                     if (!c)
                     {
                         /* TODO: forward command to other game servers through
@@ -364,13 +364,7 @@ void runCommand(Character *ch, const std::string &text)
                         say(ch, "Player " + arg + " was not found");
                         return;
                     }
-                    if (c->status != CLIENT_CONNECTED)
-                    {
-                        // No suitable character.
-                        say(ch, "Player " + arg + " is offline");
-                        return;
-                    }
-                    args[i] = (intptr_t)c->character;
+                    args[i] = (intptr_t)c;
                 }
                 break;
 

src/game-server/commandhandler.cpp
@@ -244,24 +244,6 @@ static std::string getArgument(std::string &args)
     return argument;
 }
 
-static Character* getPlayer(const std::string &player)
-{
-    // get character, via the client, as they may be
-    // on a different game server
-    GameClient *client = gameHandler->getClientByNameSlow(player);
-    if (!client)
-    {
-        return NULL;
-    }
-
-    if (client->status != CLIENT_CONNECTED)
-    {
-        return NULL;
-    }
-
-    return client->character;
-}
-
 static void handleHelp(Character *player, std::string &args)
 {
     if (args.empty())
@@ -413,7 +395,7 @@ static void handleCharWarp(Character *player, std::string &args)
     else
     {
         // check for valid player
-        other = getPlayer(character);
+        other = gameHandler->getCharacterByNameSlow(character);
         if (!other)
         {
             say("Invalid or offline character <" + character + ">.", player);
@@ -515,7 +497,7 @@ static void handleItem(Character *player, std::string &args)
     else
     {
         // check for valid player
-        other = getPlayer(character);
+        other = gameHandler->getCharacterByNameSlow(character);
         if (!other)
         {
             say("Invalid character or they are offline", player);
@@ -650,7 +632,7 @@ static void handleMoney(Character *player, std::string &args)
     else
     {
         // check for valid player
-        other = getPlayer(character);
+        other = gameHandler->getCharacterByNameSlow(character);
         if (!other)
         {
             say("Invalid character or they are offline", player);
@@ -763,7 +745,7 @@ static void handleGoto(Character *player, std::string &args)
     }
 
     // check for valid player
-    other = getPlayer(character);
+    other = gameHandler->getCharacterByNameSlow(character);
     if (!other)
     {
         say("Invalid character, or they are offline.", player);
@@ -797,7 +779,7 @@ static void handleRecall(Character *player, std::string &args)
     }
 
     // check for valid player
-    other = getPlayer(character);
+    other = gameHandler->getCharacterByNameSlow(character);
     if (!other)
     {
         say("Invalid character, or they are offline.", player);
@@ -836,7 +818,7 @@ static void handleBan(Character *player, std::string &args)
     }
 
     // check for valid player
-    other = getPlayer(character);
+    other = gameHandler->getCharacterByNameSlow(character);
     if (!other)
     {
         say("Invalid character", player);
@@ -900,7 +882,7 @@ static void handlePermissions(Character *player, std::string &args)
         return;
     }
 
-    Character *other = getPlayer(character);
+    Character *other = gameHandler->getCharacterByNameSlow(character);
     if (!other)
     {
         say("Invalid character", player);
@@ -935,7 +917,7 @@ static void handleGivePermission(Character *player, std::string &args)
     else
     {
         // check for valid player
-        other = getPlayer(character);
+        other = gameHandler->getCharacterByNameSlow(character);
         if (!other)
         {
             say("Invalid character", player);
@@ -996,7 +978,7 @@ static void handleTakePermission(Character *player, std::string &args)
     else
     {
         // check for valid player
-        other = getPlayer(character);
+        other = gameHandler->getCharacterByNameSlow(character);
         if (!other)
         {
             say("Invalid character", player);
@@ -1056,7 +1038,7 @@ static void handleAttribute(Character *player, std::string &args)
     else
     {
         // check for valid player
-        other = getPlayer(character);
+        other = gameHandler->getCharacterByNameSlow(character);
         if (!other)
         {
             say("Invalid character", player);
@@ -1163,7 +1145,7 @@ static void handleMute(Character *player, std::string &args)
 
 
     // Check for a valid player.
-    other = getPlayer(character);
+    other = gameHandler->getCharacterByNameSlow(character);
     if (!other)
     {
         say("Invalid character", player);
@@ -1229,7 +1211,7 @@ static void handleKill(Character *player, std::string &args)
     std::string character = getArgument(args);
 
     // check for valid player
-    other = getPlayer(character);
+    other = gameHandler->getCharacterByNameSlow(character);
     if (!other)
     {
         say("Invalid character", player);
@@ -1261,7 +1243,7 @@ static void handleKick(Character *player, std::string &args)
     std::string character = getArgument(args);
 
     // check for valid player
-    other = getPlayer(character);
+    other = gameHandler->getCharacterByNameSlow(character);
     if (!other)
     {
         say("Invalid character", player);
@@ -1428,7 +1410,7 @@ static void handleGetPos(Character *player, std::string &args)
         return;
     }
     Character *other;
-    other = getPlayer(character);
+    other = gameHandler->getCharacterByNameSlow(character);
     if (!other)
     {
         say("Invalid character, or they are offline.", player);
@@ -1461,7 +1443,7 @@ static void handleSkills(Character *player, std::string &args)
     if (character == "#")
         other = player;
     else
-        other = getPlayer(character);
+        other = gameHandler->getCharacterByNameSlow(character);
     if (!other)
     {
         say("Invalid character, or they are offline.", player);
@@ -1504,7 +1486,7 @@ static void handleEffect(Character *player, std::string &args)
     else if (arguments.size() == 2)
     {
         int id = utils::stringToInt(arguments[0]);
-        Character *p = getPlayer(arguments[1]);
+        Character *p = gameHandler->getCharacterByNameSlow(arguments[1]);
         if (!p)
         {
             say("Invalid target player.", player);

src/game-server/gamehandler.h
@@ -106,10 +106,10 @@ class GameHandler: public ConnectionHandler
         void deletePendingConnect(Character *character);
 
         /**
-         * Gets the client associated to a character name. This method is slow,
-         * so it should never be called for regular operations.
+         * Gets the character associated to a character name. This method is
+         * slow, so it should never be called for regular operations.
          */
-        GameClient *getClientByNameSlow(const std::string &) const;
+        Character *getCharacterByNameSlow(const std::string &) const;
 
     protected:
         NetComputer *computerConnected(ENetPeer *);

src/scripting/lua.cpp
@@ -1547,6 +1547,23 @@ static int get_beings_in_rectangle(lua_State *s)
  }
 
 /**
+ * get_character_by_name(string name): Character*
+ * Returns the character handle or NULL if there is none
+ */
+static int get_character_by_name(lua_State *s)
+{
+    const char *name = luaL_checkstring(s, 1);
+
+    Character *ch = gameHandler->getCharacterByNameSlow(name);
+    if (!ch)
+        lua_pushnil(s);
+    else
+        lua_pushlightuserdata(s, ch);
+
+    return 1;
+}
+
+/**
  * chr_get_post(Character*): void
  * Gets the post for the character.
  */
@@ -2260,6 +2277,7 @@ LuaScript::LuaScript():
         { "chat_message",                    &chat_message                    },
         { "get_beings_in_circle",            &get_beings_in_circle            },
         { "get_beings_in_rectangle",         &get_beings_in_rectangle         },
+        { "get_character_by_name",           &get_character_by_name           },
         { "being_register",                  &being_register                  },
         { "effect_create",                   &effect_create                   },
         { "chr_shake_screen",                &chr_shake_screen                },

commit 2f11d4c109bb0f61c9f6d26c9ebc9d0cdd3a1a92

Fixed finding characters by name
This was broken in ba66fbeda8ef9afb6c33eba66d109bac85ebf628 where we forgot
to make sure to invert the condition when moving it into the function.

Reviewed-by: Erik Schilling

src/game-server/gamehandler.cpp
@@ -391,7 +391,7 @@ Character *GameHandler::getCharacterByNameSlow(const std::string &name) const
         GameClient *c = static_cast< GameClient * >(*i);
         Character *ch = c->character;
         if (ch && ch->getName() == name &&
-                c->status != CLIENT_CONNECTED)
+                c->status == CLIENT_CONNECTED)
         {
             return ch;
         }