c93dbdd5c1349f66d9075d716cc8f5a4eece89aa
Erik Schilling <ablu.erikschilling@googlemail.com>
2013-04-20 22:26:24 +0200

    [Abilities] Removed all except one ability
    Should allow easier modification without thinking of useful examples for
    each one

--- example/scripts/abilities.lua
+++ example/scripts/abilities.lua
@@ -15,8 +15,3 @@ spell1:on_use(function(user, x, y, abilityId)
     user:set_ability_mana(abilityId, 0)
 end)
 spell1:on_recharged(function(ch) ch:say("Hoooooooo...") end)
-
-local spell2 = get_ability_info(2)
-spell2:on_use(function(user) user:say("HAA-DOKEN!") end)
-
-get_ability_info(3):on_use(function(user) user:say("Sonic BOOM") end)
