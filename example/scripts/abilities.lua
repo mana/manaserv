10c33df8e5a13e7e7b4a3ea203516536582b2f4b
Erik Schilling <ablu.erikschilling@googlemail.com>
2013-04-22 09:21:01 +0200

    [Abilities] Added a autoconsume option
    You do not need to reset the mana in the scripts if you do not want to.
    We will need this for attacks later since those do not nessecary have a
    script.

--- example/scripts/abilities.lua
+++ example/scripts/abilities.lua
@@ -12,6 +12,5 @@ local spell1 = get_ability_info("Magic_Test Spell 1")
 spell1:on_use(function(user, x, y, abilityId)
     target = target or user
     target:say("Kaaame...Haaame... HAAAAAA! " .. x .. " " .. y)
-    user:set_ability_mana(abilityId, 0)
 end)
 spell1:on_recharged(function(ch) ch:say("Hoooooooo...") end)
